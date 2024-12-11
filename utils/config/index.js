6247ba619340f25d7422e621552c02a63c29e9db
Abialidr <luckybelt79@gmail.com>
2024-12-12 00:59:02 +0530

    a

--- utils/config/index.js
+++ utils/config/index.js
@@ -1,5 +1,5 @@
 // export const PROXY = "http://localhost:5000";
 export const PROXY =
-  "https://test.makemymattress.co.in/";
+  "https://test.makemymattress.co.in";
 export const S3PROXY =
   "https://makemymatressbucket123.s3.ap-south-1.amazonaws.com";
